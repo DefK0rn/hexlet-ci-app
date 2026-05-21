702b1e39a1647999cc18daef35dbd440cb809278
DefK0rn <defkorn@yandex.ru>
2026-05-21 22:27:35 +0300

    Fix database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'better-sqlite3',
     connection: {
       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
