66add980e31e5cc44231a34bd545f501915fb84a
DefK0rn <defkorn@yandex.ru>
2026-05-21 22:02:16 +0300

    Fix strapi.js

--- tests/helpers/strapi.js
+++ tests/helpers/strapi.js
@@ -1,33 +1,24 @@
-const Strapi = require('@strapi/strapi');
-const fs = require('fs');
+const createStrapi = require('@strapi/strapi'); // В v5 функция называется createStrapi
 
 let instance;
 
 async function setupStrapi() {
   if (!instance) {
-    await Strapi().load();
-    instance = strapi; // eslint-disable-line
-
-    await instance.server.mount();
+    // В v5 вместо Straполнительного вызова .load() используется фабрика createStrapi
+    instance = await createStrapi().load();
+    
+    // В v5 сервер монтируется автоматически при загрузке, 
+    // но если тестам нужен HTTP-сервер, его запускают так:
+    await instance.server.listen(); 
   }
   return instance;
 }
 
 async function cleanupStrapi() {
-  const dbSettings = strapi.config.get('database.connections.default.settings'); // eslint-disable-line
-
-  // close server to release the db-file
-  await strapi.server.httpServer.close(); // eslint-disable-line
-
-  // delete test database after all tests have completed
-  if (dbSettings && dbSettings.filename) {
-    const tmpDbFile = `${__dirname}/../${dbSettings.filename}`;
-    if (fs.existsSync(tmpDbFile)) {
-      fs.unlinkSync(tmpDbFile);
-    }
+  if (instance) {
+    // Закрываем сервер и подключение к БД стандартным методом деструкции v5
+    await instance.destroy();
   }
-  // close the connection to the database
-  await strapi.db.connection.destroy(); // eslint-disable-line
 }
 
 module.exports = { setupStrapi, cleanupStrapi };
